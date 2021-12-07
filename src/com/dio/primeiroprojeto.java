2d8cb1dfd0ad4d5a126a15b3e6a355103b61d512
profnatal <prof.nataljhonkleiton@gmail.com>
2021-12-06 23:16:24 -0400

    first commit

---
+++ src/com/dio/primeiroprojeto.java
@@ -0,0 +1,30 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class primeiroprojeto {
+
+	public static void main(String[] args) {
+			
+		Gato gato = new Gato(); 
+		Livros Livros = new Livros ();
+		
+		
+		System.out.println(gato);
+		System.out.println(Livros);
+		
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello world! " + (a+b));*/
+		
+	}
+
+}
+
+class Livros { 
+	private String name;
+	private String npag;
+	
+	
+}
